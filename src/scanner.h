4b318753cc64eb33cea8ccfa73de755c51850fdc
suru-ach <srujanacharya36@gmail.com>
2024-04-30 13:11:41 +0530

    from v3

--- src/scanner.h
+++ src/scanner.h
@@ -1,40 +1,41 @@
-#include<string>
-#include<vector>
-#include<unordered_map>
-#include "tokens.h"
+#ifndef SCANNER_H_
+#define SCANNER_H_
 
+#include "token.h"
+#include <string>
+#include <unordered_map>
+#include <vector>
 using std::string;
 using std::vector;
 using std::unordered_map;
 
-
 class Scanner {
-	private:
-		string source;
-		vector<Token> tokens; 
-		int start = 0;
-		int current = 0;
-		int line = 1;
+ private:
+    string source;
+    vector<Token> tokens;
+    static unordered_map <string, TokenType> keywords;
+    int start = 0;
+    int current = 0;
+    int line = 1;
 
-	public:
-		Scanner(string sourceFile) {
-			source = sourceFile;
-		}
-		vector<Token> scanTokens();
+    void scanToken();
+    char advance();
+    void addToken(TokenType type);
+    void addToken(TokenType type, Object literal);
+    bool match(char expected);
+    char peek();
+    void generateString();
+    void generateNumber();
+    bool isDigit(char c);
+    char peekNext();
+    void identifier();
+    bool isAlpha(char c);
+    bool isAlphaNumeric(char c);
 
-		// scanToken method to scan each character and add it to respective list  
-		void scanToken();
-		void identifier();
-		bool isAlpha(char c);
-		bool isAlphaNumeric(char c);
-		bool isDigit(char c);
-		void number();
-		char peekNext();
-		char peek();
-		bool match(char expected);
-		// Helper: to check if at end
-		bool isAtEnd();
-		char advance();
-		void addToken(TokenType type);
-		void String();
+ public:
+    explicit Scanner(string source);
+    vector<Token> scanTokens();
+    bool isAtEnd();
 };
+
+#endif // !SCANNER_H_
